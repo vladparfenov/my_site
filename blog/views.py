d001d628ec93154ba25b2873391b898cea9e68cd
vladparfenov <vladdor.v@yandex.ru>
2020-10-26 00:17:02 +0300

    correct commit

--- blog/views.py
+++ blog/views.py
@@ -29,5 +29,15 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
-def post_new():
-    return None
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
